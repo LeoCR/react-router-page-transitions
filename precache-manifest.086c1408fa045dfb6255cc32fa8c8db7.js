a7339c6995853f1f0ddee06b9902adb60a3b07e5
Leonardo Aranibar <leo@iMac-de-Leonardo.local>
2018-12-23 11:26:24 -0600

    Deploy to githubpages.com

---
+++ precache-manifest.086c1408fa045dfb6255cc32fa8c8db7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a30cf7612315f2b0399a",
+    "url": "/react-router-page-transitions/static/js/runtime~main.a30cf761.js"
+  },
+  {
+    "revision": "d1142144ff0adfb9b62c",
+    "url": "/react-router-page-transitions/static/js/main.d1142144.chunk.js"
+  },
+  {
+    "revision": "ceabcd750c864c652fdd",
+    "url": "/react-router-page-transitions/static/js/1.ceabcd75.chunk.js"
+  },
+  {
+    "revision": "d1142144ff0adfb9b62c",
+    "url": "/react-router-page-transitions/static/css/main.7ef44b56.chunk.css"
+  },
+  {
+    "revision": "542899aefa36b40e8d5a3beb70def3a8",
+    "url": "/react-router-page-transitions/index.html"
+  }
+];
